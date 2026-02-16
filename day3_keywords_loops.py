82bb2c7ae0ef0182be28d7d344bfdcef3bf225c4
Rakesh saini <indoreyarakesh77@gmail.com>
2026-02-16 23:54:19 +0530

    Create day3_keywords_loops.py

---
+++ day3_keywords_loops.py
@@ -0,0 +1,20 @@
+print("---- Day 3 Practice ----")
+
+# Identifiers
+name = "Rakesh"
+age = 17
+
+print("Name:", name)
+print("Age:", age)
+
+print("\nFor Loop Example:")
+for i in range(5):
+    print(i)
+
+print("\nWhile Loop Example:")
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+
+
