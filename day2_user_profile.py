a8ac78785210b1850fb2b7c7a091e672ad72a974
Rakesh saini <indoreyarakesh77@gmail.com>
2026-02-17 22:31:04 +0530

    Day 2_ variables _typing concepts

--- day2_user_profile.py
+++ day2_user_profile.py
@@ -1,25 +1,83 @@
-# Day 2 - Variables, Input, Type Casting, If-Else
+## Day 2 
+# Topice covered:
+# 1.comments
+# 2.variables
 
-print("---- User Profile Generator ----")
+# coments
 
-# Taking user input
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-state = input("Enter your state: ")
-fav_language = input("Enter your favorite programming language: ")
+# This is a single line comment
 
-# Processing
-age_after_5_years = age + 5
+# Program to print a message
+print("Hello Rakesh")
 
-print("\n---- User Profile ----")
-print(f"Name: {name}")
-print(f"Age after 5 years: {age_after_5_years}")
-print(f"State: {state}")
-print(f"Favorite Language: {fav_language}")
 
-# Eligibility check
-if age >= 18:
-    print("You are eligible to vote.")
-else:
-    print("You are not eligible to vote.")
+
+# Inline comment example
+
+age = 17  # Storing age value
+name = "Rakesh"  # Storing name
+
+print("Name:", name)
+print("Age:", age)
+
+
+"""
+This is a multi-line comment example.
+It explains the purpose of this program.
+Python ignores this block during execution.
+"""
+
+print("This program demonstrates multi-line comments.")
+
+
+## 2. variable and Typing 
+
+# Variable Declaration
+a = 5
+b = 6
+c = 7
+
+print("Value of a:", a)
+print("Value of b:", b)
+print("Value of c:", c)
+
+# Multiple Assignment
+x, y, z = 4, 5, 6
+
+print("Value of x:", x)
+print("Value of y:", y)
+print("Value of z:", z)
+
+
+# Python is dynamically typed
+num = 10
+print("num:", num, "Type:", type(num))
+
+num = "Rakesh"
+print("num:", num, "Type:", type(num))
+
+
+
+
+
+#  Simple Function (Static Binding concept explanation)
+def greet():
+    print("Hello Rakesh")
+
+greet()
+
+
+# - Dynamic Binding (Method Overriding)
+
+class A:
+    def show(self):
+        print("Class A")
+
+class B(A):
+    def show(self):
+        print("Class B")
+
+obj = B()
+obj.show()   # Runtime decides which method to call
+
 
