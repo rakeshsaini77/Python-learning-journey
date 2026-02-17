d649182cd1085d5dc829bdbddcac2f9158f21752
Rakesh saini <indoreyarakesh77@gmail.com>
2026-02-17 22:38:28 +0530

    day3_keywords_ldentifiers.py

---
+++ day3_keywords_ldentifiers.py
@@ -0,0 +1,47 @@
+
+## Day 3  topice covered
+# 1.Keywords
+# 2.Identifiers
+
+
+##  Keywords:
+
+# Keywords are reserved words in Python
+# We cannot use them as variable names
+
+# Example:
+# if = 10   # ❌ This will give error (because 'if' is a keyword)
+
+print("Some Python Keywords:")
+print("if, else, for, while, True, False, None, def, class")
+
+
+
+##  Identifiers Example
+# Identifiers are names given to variables, functions, classes etc.
+
+# Valid Identifiers
+name = "Rakesh"
+_age = 17
+total_marks = 95
+
+print("\nValid Identifiers:")
+print("Name:", name)
+print("Age:", _age)
+print("Total Marks:", total_marks)
+
+
+# Invalid Identifiers (Examples - do not run)
+# 1name = "Rakesh"    ❌ Cannot start with number
+# user-name = "RS"    ❌ Hyphen not allowed
+# class = 10          ❌ 'class' is a keyword
+
+
+print("1. Must start with a letter or underscore")
+print("2. Cannot start with a number")
+print("3. Cannot use special symbols")
+print("4. Cannot use Python keywords")
+
+
+
+
