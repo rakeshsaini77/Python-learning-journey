68c3ae79d447a1a9749d258f628e6e300f78394d
Rakesh saini <indoreyarakesh77@gmail.com>
2026-02-17 23:04:32 +0530

    Create day4_userinput_typeconversion.py

---
+++ day4_userinput_typeconversion.py
@@ -0,0 +1,40 @@
+## Day 4 topice covered:
+# 1.Taking user input
+# 2.Type conversion
+
+# Taking user input
+variable_name = input("Message to user: ")
+
+# Taking name as input
+
+name = input("Enter your name: ")
+
+print("Hello", name)
+
+
+# integer input
+age = int(input("Enter your age: "))
+
+print("Your age is:", age)
+
+#float input
+price = float(input("Enter product price: "))
+
+print("Price is:", price)
+
+## Type conversion
+
+# Taking input
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+# Converting string input to integer
+num1 = int(num1)
+num2 = int(num2)
+
+# Performing addition
+result = num1 + num2
+
+print("Sum is:", result)
+
+print("Data type of result:", type(result))
