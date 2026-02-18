69f34db224eef1403bb9e7e4c778e1075dc89863
Rakesh saini <indoreyarakesh77@gmail.com>
2026-02-18 23:59:09 +0530

    day4_type_conversion.py

---
+++ day5_type_conversion.py
@@ -0,0 +1,48 @@
+## Topic
+#1.Type Conversion
+
+
+#  String to Integer
+num_str = "100"
+num_int = int(num_str)
+
+print("\nString to Integer:")
+print("Value:", num_int)
+print("Type:", type(num_int))
+
+
+#  Integer to Float
+num = 50
+num_float = float(num)
+
+print("\nInteger to Float:")
+print("Value:", num_float)
+print("Type:", type(num_float))
+
+
+#  Number to String
+age = 21
+age_str = str(age)
+
+print("\nNumber to String:")
+print("My age is " + age_str)
+print("Type:", type(age_str))
+
+
+#  String to Float
+price_str = "99.99"
+price_float = float(price_str)
+
+print("\nString to Float:")
+print("Value:", price_float)
+print("Type:", type(price_float))
+
+
+#  Boolean Conversion
+print("\nBoolean Conversion:")
+print("bool(1):", bool(1))
+print("bool(0):", bool(0))
+print("bool('Hello'):", bool("Hello"))
+print("bool(''):", bool(""))
+
+
