d7c39e2c01fbc411b56135d699f5d7c1fe143895
Rakesh saini <indoreyarakesh77@gmail.com>
2026-02-21 22:51:49 +0530

    day8_whileloop_forloop.py

---
+++ day8_whileloop_forloop.py
@@ -0,0 +1,28 @@
+# Multiplication Table using While Loop
+
+number = int(input("Enter the number: "))
+
+i = 1
+while i <= 10:
+    print(number * i)
+    i += 1
+
+
+
+for loop:
+# Print numbers from 1 to 10
+
+for i in range(1, 11):
+    print(i)
+
+# Reverse counting
+
+for i in range(10, 0, -1):
+    print(i)
+
+
+print(list(range(1, 10)))
+print(list(range(15)))
+print(list(range(1, 11, 3)))
+print(list(range(10, 0, -1)))
+
